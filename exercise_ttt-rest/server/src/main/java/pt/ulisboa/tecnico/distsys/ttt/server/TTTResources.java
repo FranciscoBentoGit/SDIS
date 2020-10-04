67a2c1278f279dce0f4e97678153e4aac475904b
Joao-Pedro-Lopes <joaopedrolopes00@gmail.com>
2020-10-05 00:59:30 +0100

    falta iv

---
+++ exercise_ttt-rest/server/src/main/java/pt/ulisboa/tecnico/distsys/ttt/server/TTTResources.java
@@ -0,0 +1,72 @@
+package pt.ulisboa.tecnico.distsys.ttt.server;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import pt.ulisboa.tecnico.distsys.ttt.contract.PlayResult;
+import pt.ulisboa.tecnico.distsys.ttt.contract.PlayRequest;
+
+/**
+ * Root resource (exposed at "game" path)
+ */
+@Path("game")
+public class TTTResources {
+	
+	/**
+	 * 
+	 * TTTGame currently being played
+	 * 
+	 */
+	static TTTGame game = new TTTGame();
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return Board that will be returned as a text/plain response.
+     */ 
+    @GET
+    @Path("board")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getBoard() {
+    	return game.toString();
+    }
+    
+    
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return Board that will be returned as a text/plain response.
+     */ 
+    @GET
+    @Path("board/reset")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String resetBoard() {
+        game.resetBoard();
+        return game.toString();
+    }
+    
+    
+    /* TODO: checkwinner, play */
+    @GET
+    @Path("board/checkwinner")
+    @Produces(MediaType.TEXT_PLAIN)
+    public int checkWinner() {
+    	return game.checkWinner();
+    }
+    
+    @POST
+    @Path("play")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public PlayResult play(PlayRequest pR) {
+    	return game.play(pR.getRow(), pR.getColumn(), pR.getPlayer());
+    }
+    
+}
